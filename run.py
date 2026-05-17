fc51d7a626f12531cac16dc83e862c78b07bb609
Jairo Cabrera <jairocabrera87@hotmail.com>
2026-05-17 17:22:39 -0500

    Update run.py

--- run.py
+++ run.py
@@ -5,7 +5,7 @@ from utils.web_scrapping import scrape_all_movies_parallel
 def main():
   print('Downloading files...')
   config = load_config("config/config.yaml")
-  download_dataset(config.get('data'))
+  #download_dataset(config.get('data'))
   scrape_all_movies_parallel(config.get('data'), max_workers=6)
   print('Fin del programa')
 
