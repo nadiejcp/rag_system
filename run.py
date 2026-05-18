dbbb093ae7df11a483b704dbef2310b8247fa4a2
Nicolas Carrion <ncarrion@estud.usfq.edu.ec>
2026-05-17 19:23:42 -0500

    Update run.py

--- run.py
+++ run.py
@@ -1,12 +1,21 @@
-from utils.download import download_dataset
+# from utils.download import download_dataset
 from config.load_config import load_config
 from utils.web_scrapping import scrape_all_movies_parallel
+from vector_store import build_embeddings
 
 def main():
   print('Downloading files...')
-  config = load_config("config/config.yaml")
+  config = load_config()
   #download_dataset(config.get('data'))
-  scrape_all_movies_parallel(config.get('data'), max_workers=6)
+
+  if input('¿Deseas construir la base de datos raw? (s/n): ').lower() == 's':
+    print('Iniciando web scrapping y almacenando datos raw...')
+    scrape_all_movies_parallel(config.get('data'), max_workers=6)
+
+  if input('¿Deseas construir la base de datos vectorial? (s/n): ').lower() == 's':
+    print('Generando embeddings y almacenando en la base de datos vectorial...')
+    build_embeddings(config)
+  
   print('Fin del programa')
 
 if __name__ == "__main__":
